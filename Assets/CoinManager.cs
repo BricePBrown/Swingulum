893a35041e3722be16202919b9769bf1351123a1
Brice Brown <BriceBro@usc.edu>
2023-11-28 20:54:50 -0800

    minor adjustments

--- Assets/CoinManager.cs
+++ Assets/CoinManager.cs
@@ -4,12 +4,33 @@ using UnityEngine;
 
 public class CoinManager : MonoBehaviour
 {
+    private bool hasPlayedSound = false;
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.name.Equals("Player")){
-            this.GetComponent<AudioSource>().Play();
-            FindAnyObjectByType<CoinTracker>().updateCoins();
-            this.gameObject.SetActive(false);
+        if (!hasPlayedSound && other.gameObject.name.Equals("Player"))
+        {
+            // Play the sound
+            AudioSource audioSource = GetComponent<AudioSource>();
+            if (audioSource != null)
+            {
+                audioSource.Play();
+            }
+
+            // Move the coin up by 100 units
+            transform.Translate(Vector3.up * 100f);
+
+            // Set a flag to prevent playing the sound again
+            hasPlayedSound = true;
+
+            // Deactivate the GameObject after the sound finishes (adjust the delay as needed)
+            float soundDuration = audioSource.clip.length;
+            Invoke("DeactivateCoin", soundDuration);
         }
+    }
+
+    private void DeactivateCoin()
+    {
+        gameObject.SetActive(false);
     }
 }
